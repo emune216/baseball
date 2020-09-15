e75cd6aebd98dad09f535a97102fd383aa223b9e
yuns94 <than4502@gmail.com>
2020-09-16 00:36:03 +0900

    value.length = 3

--- randomNum.js
+++ randomNum.js
@@ -1,20 +1,54 @@
 const input = document.querySelector("input"),
   check = document.querySelector(".check"),
-  runRandom = getRandom();
+  runRandom = getRandom(); // 랜덤숫자가 페이지 새로고침 전까지 고정된다.
 
-function compareNum(ranNum) {
-  const answer = input.value;
-  console.log(answer);
-  console.log(ranNum);
+function inputCheck() {
+  const value = input.value;
+  if (value.length > 3) {
+    alert("숫자 3개를 입력해주세요💦");
+  } else if (value.length < 3) {
+    alert("숫자 3개를 입력해주세요💦");
+  } else {
+    compareNum(value);
+  }
+}
+
+function compareNum(num) {
+  console.log(num);
+  console.log(runRandom);
 }
 
 function getRandom() {
   const ranNum = Math.floor(Math.random() * 1000);
-  compareNum(ranNum);
+  return ranNum;
 }
 
 function init() {
-  check.addEventListener("click", compareNum);
+  check.addEventListener("click", inputCheck);
 }
 
 init();
+
+/* 
+function inputCheck() {
+  const value = input.value;
+
+  switch (true) {
+    case value > 1000:
+      alert("세 자리 숫자로 입력해주세요💦");
+      return false;
+    case value < 10:
+      answer = "00" + value;
+      break;
+    case answer < 100:
+      answer = "0" + value;
+      break;
+    default:
+      answer = value;
+  }
+  compareNum(answer);
+
+  function compareNum(num){
+      console.log(num);
+  }
+  */
